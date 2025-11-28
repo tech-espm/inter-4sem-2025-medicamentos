52e09c7a7007d07a31b89d745ec20f341e0f152b
Maria Gabriela <87874439+mgabriel4@users.noreply.github.com>
2025-11-27 22:11:07 -0300

    Remoção das duplicatas na tabela Marca.

--- raspagemMarca.py
+++ raspagemMarca.py
@@ -4,29 +4,40 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC
-# import pandas as pd
 import config
 
 engine = create_engine(config.conexao_banco)
 
 driver = webdriver.Chrome()
 driver.set_page_load_timeout(300)
-dados = [] 
 
 for n in range(1, 9):
-	with Session(engine) as sessao, sessao.begin():
-		driver.get(f"{config.urlbase}?p={n}")
-		marcas = [m.text for m in WebDriverWait(driver, 20).until(
-			EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.product-brand"))
-		)]                                                                   
-		for marca in marcas:
 
-			laboratorio ={
-				'nomeMarca': marca
-			}
-			sessao.execute(text("INSERT INTO marca (nomeMarca) VALUES (:nomeMarca)"), laboratorio)
+    with Session(engine) as sessao, sessao.begin():
 
-# df = pd.DataFrame(dados)
-# df.to_csv("saida.csv", index=False, encoding="utf-8-sig")
+        driver.get(f"{config.urlbase}?p={n}")
+
+        marcas = [m.text.strip() for m in WebDriverWait(driver, 20).until(
+            EC.presence_of_all_elements_located(
+                (By.CSS_SELECTOR, "div.product-brand"))
+        )]
+
+        for nome_marca in marcas:
+
+            # é necessário verificar se já existe no banco
+            id_existente = sessao.execute(
+                text("SELECT idmarca FROM marca WHERE nomeMarca = :nome"),
+                {"nome": nome_marca}
+            ).scalar()
+
+            # se a marca já existir, pula
+            if id_existente:
+                continue
+
+            # se não existi, insere
+            sessao.execute(
+                text("INSERT INTO marca (nomeMarca) VALUES (:nome)"),
+                {"nome": nome_marca}
+            )
 
 driver.quit()
